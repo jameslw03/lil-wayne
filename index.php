ccd77ef5ce6249b2fa68eeda2746304452b60ff6
jameslw03 <jameslw2003@gmail.com>
2025-03-11 14:02:38 +0000

    quote carousel, rapper stats and careter album series

--- index.php
+++ index.php
@@ -133,21 +133,17 @@
     <p>See All</p>
     <img src="assets/svg's/red-right-arrow.svg" alt="Arrow to indicate next page">
   </a>
-
-  <h2 class="h1">Bio:</h2> 
+  <h2 class="h1">Bio:</h2>
   <div class="bio-section">
     <img src="assets/svg's/fear.svg" alt="Fear tattoo from Lil Waynes eyelid" class="fear-tattoo">
-    <article>
-        <h3 class="name">Name</h3>
-        <p>Dwayne Michael Carter Jr., known professionally as Lil Wayne.</p>
-        
-        <h3 class="age">Age</h3>
-        <p>Born: 27 September 1982 (age 42), New Orleans, Louisiana, United States.</p>
-        
-        <h3 class="record-labels">Record Labels</h3>
-        <p>Young Money Entertainment.</p>
-        
-        <p class="description">Lil Wayne is an American rapper. He is often regarded as one of the most influential hip hop artists of his generation, as well as one of the greatest rappers of all time.</p>
+    <article class="bio-details">
+      <h3 class="name">Name</h3>
+      <p>Dwayne Michael Carter Jr., known professionally as Lil Wayne.</p>
+      <h3 class="age">Age</h3>
+      <p>Born: 27 September 1982 (age 42), New Orleans, Louisiana, United States.</p>
+      <h3 class="record-labels">Record Labels</h3>
+      <p>Young Money Entertainment.</p>
+      <p class="description">Lil Wayne is an American rapper. He is often regarded as one of the most influential hip hop artists of his generation, as well as one of the greatest rappers of all time.</p>
     </article>
     <div class="rapper-rating">
       <div class="overall-rating">
@@ -158,36 +154,157 @@
         </div>
       </div>
       <div class="rating-breakdown">
-      <article class="rating-card">
-            <h3>Lyricism</h3>
-            <p>Measures wordplay, flow, storytelling, and lyrical creativity.</p>
-            <p class="score">23/25</p>
-            <img src="assets/svg's/rating-bar-lyricism.svg" alt="Rating bar that shows 23/25" class="rating-bar">
-        </article>
-        
-        <article class="rating-card">
-            <h3>Influence</h3>
-            <p>Evaluates cultural impact, legacy, and industry reach.</p>
-            <p class="score">25/25</p>
-            <img src="assets/svg's/rating-bar-full.svg" alt="Rating bar that shows 25/25" class="rating-bar">
-        </article>
-
-        <article class="rating-card">
-            <h3>Discography</h3>
-            <p>Rates the quality and consistency of albums and features.</p>
-            <p class="score">22.5/25</p>
-            <img src="assets/svg's/rating-bar-discography.svg" alt="Rating bar that shows 22.5/25" class="rating-bar">
-        </article>
-        
-        <article class="rating-card">
-            <h3>Creativity</h3>
-            <p>Assesses innovation, artistic vision, and originality.</p>
-            <p class="score">25/25</p>
-            <img src="assets/svg's/rating-bar-full.svg" alt="Rating bar that shows 25/25" class="rating-bar">
-        </article>
+        <div class="column-one">
+          <article class="rating-card lyricism">
+            <img src="assets/svg's/microphone.svg" alt="Microphone to represent lyricism and rapping" class="microphone">
+            <div class="rating-card-text">
+              <h3>Lyricism</h3>
+              <p>Measures wordplay, flow, storytelling, and lyrical creativity.</p>
+            </div>
+            <div class="rating-card-rating">
+              <p class="score">23/25</p>
+              <img src="assets/svg's/rating-bar-lyricism.svg" alt="Rating bar that shows 23/25" class="rating-bar">
+            </div>
+          </article>
+          <article class="rating-card influence">
+            <img src="assets/svg's/world.svg" alt="World to represent influence" class="world">
+            <div class="rating-card-text">
+              <h3>Influence</h3>
+              <p>Evaluates cultural impact, legacy, and industry reach.</p>
+            </div>
+            <div class="rating-card-rating">
+              <p class="score">25/25</p>
+              <img src="assets/svg's/rating-bar-full.svg" alt="Rating bar that shows 25/25" class="rating-bar">
+            </div>
+          </article>
+        </div>
+        <div class="column-two">
+          <article class="rating-card discography">
+            <img src="assets/svg's/music-symbol.svg" alt="Music symbol to represent music catalogue" class="music-symbol">
+            <div class="rating-card-text">
+              <h3>Discography</h3>
+              <p>Rates the quality and consistency of albums and features.</p>
+            </div>
+            <div class="rating-card-rating">
+              <p class="score">22.5/25</p>
+              <img src="assets/svg's/rating-bar-discography.svg" alt="Rating bar that shows 22.5/25" class="rating-bar">
+            </div>
+          </article>
+          <article class="rating-card creativity">
+            <img src="assets/svg's/light-bulb.svg" alt="Lightbulb to represent creativity and ideas" class="light-bulb">
+            <div class="rating-card-text">
+              <h3>Creativity</h3>
+              <p>Assesses innovation, artistic vision, and originality.</p>
+            </div>
+            <div class="rating-card-rating">
+              <p class="score">25/25</p>
+              <img src="assets/svg's/rating-bar-full.svg" alt="Rating bar that shows 25/25" class="rating-bar">
+            </div>
+          </article>
+        </div>
       </div>
     </div>
   </div>
+  <a href="#" class="see-more-articles">
+    <p>See All</p>
+    <img src="assets/svg's/red-right-arrow.svg" alt="Arrow to indicate next page">
+  </a>
+
+  <div class="quote-section">
+    <div class="svg-wrapper">
+      <img src="assets/svg's/c-tattoo.svg" alt="C tattoo from center of Lil Wayne's forehead" class="c-tattoo">
+    </div>
+    <div class="quote-container">
+      <div class="quote-wrapper">
+        <p class="quote">“I tried to pay attention but attention payed me”</p>
+        <p class="credit">She Will | Tha Carter 4</p>
+      </div>
+      <div class="quote-wrapper">
+        <p class="quote">“Jumped in the pool with her and her friend, none of us swim”</p>
+        <p class="credit">Slip | Tha Fix Before Tha 6</p>
+      </div>
+      <div class="quote-wrapper">
+        <p class="quote">“Didn't wear bullet proof and so I got shot and you can see the proof”</p>
+        <p class="credit">Mr.Carter | Tha Carter 3</p>
+      </div>
+      <div class="quote-wrapper">
+        <p class="quote">“Freedom was my girl until they f***ing took her”</p>
+        <p class="credit">Blunt Blowing | Tha Carter 4</p>
+      </div>
+    </div>
+  </div>
+  <h2 class="h1 discography">Discography:</h2>
+
+  <div class="tha-carter-series">
+    <h3 class="carter-series-title">Tha Carter Series</h3>
+    <div class="column-one">
+      <div class="flip-card carter-one">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="assets/images/carter-series/tha-carter-1.jpg" alt="Carter 1 album cover">
+          </div>
+          <div class="flip-card-back">
+            <img src="assets/images/carter-series/tha-carter-1-back.jpg" alt="Carter 1 back cover">
+          </div>
+        </div>
+      </div>
+      <div class="flip-card carter-two">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="assets/images/carter-series/tha-carter-2.jpg" alt="Carter 2 album cover">
+          </div>
+          <div class="flip-card-back">
+            <img src="assets/images/carter-series/tha-carter-2-back.jpg" alt="Carter 2 back cover">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column-two">
+      <div class="flip-card carter-three">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="assets/images/carter-series/tha-carter-3.jpg" alt="Carter 3 album cover">
+          </div>
+          <div class="flip-card-back">
+            <img src="assets/images/carter-series/tha-carter-3-back.jpg" alt="Carter 3 back cover">
+          </div>
+        </div>
+      </div>
+      <div class="flip-card carter-four">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="assets/images/carter-series/tha-carter-4.jpg" alt="Carter 4 album cover">
+          </div>
+          <div class="flip-card-back">
+            <img src="assets/images/carter-series/tha-carter-4-back.jpg" alt="Carter 4 back cover">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column-three">
+      <div class="flip-card carter-five">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="assets/images/carter-series/tha-carter-5.jpg" alt="Carter 5 album cover">
+          </div>
+          <div class="flip-card-back">
+            <img src="assets/images/carter-series/tha-carter-5-back.jpg" alt="Carter 5 back cover">
+          </div>
+        </div>
+      </div>
+      <div class="flip-card carter-six">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="assets/images/carter-series/tha-carter-6.jpg" alt="Carter 6 album cover">
+          </div>
+          <div class="flip-card-back">
+            <img src="assets/images/carter-series/tha-carter-6-back.jpg" alt="Carter 6 back cover">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <footer>
     <script src="assets/scripts/date-updater.js" defer></script>
   </footer>
